1a132a78e3726cd3e7dcac68d54311eceaae6ba1
Zach Connelly <zmconnelly@gmail.com>
2016-10-18 14:21:20 -0600

    Logicz

--- LogicExplorer.java
+++ LogicExplorer.java
@@ -1,9 +1,8 @@
-
 public class LogicExplorer {
 
     private World world;
     private KnowledgeBase kb;
-    int arrowCount;
+    private int arrowCount;
 
     private final byte BREEZE = 0b00000001;
     private final byte STENTCH = 0b0000010;
@@ -20,26 +19,77 @@ public class LogicExplorer {
         this.arrowCount = world.arrowCount;
     }
 
+    private void move(int action) {
+
+        if (action == 2) {
+            kb.tell(encodePercepts(world.action(action)));
+        } else if (action == 5){
+            arrowCount--;
+            world.action(action);
+        } else {
+            world.action(action);
+        }
+    }
+
+    private String encodePercepts(int percepts) {
+
+        String sentence = "";
+
+        //conversion logic goes here
+
+        return sentence;
+    }
+
     public void decideAction(byte percepts) {
         if ((percepts & GLITTER) != 0) {//maybe just kb.ask("Holding(Gold,Result(Grab,CurrentPosition))"): is better, no percept based logic within agent.
-            //action(Grab);
+            move(1);    //grab gold and end game
         } else if (kb.ask("Safe(Result(Move,CurrentPosition))&&!Explored(Result(Move,CurrentPosition))")) {
-            //action(Move);
+            move(2);    //move forward
         } else if (kb.ask("Safe(Result(Move,Result(TurnLeft,CurrentPosition))&&!Explored(Result(Move,Result(TurnLeft,CurrentPosition))")) {
-            //action(TurnLeft)
+            move(3);    //turn left
+            move(2);    //move forward
         } else if (kb.ask("Safe(Result(Move,Result(TurnRight,CurrentPosition))&&!Explored(Result(Move,Result(TurnRight,CurrentPosition))")) {
-            //action(TurnLeft)
+            move(4);    //turn right
+            move(2);    //move forward
         } else if (kb.ask("EXIST(s), Safe(s) && !Explored(s)")) {
-            //follow right hand wall
+            RHWTraversal("I am adjacent to a space that is unexplored and safe");
+            move(2);
         } else {
             if (arrowCount > 0) {
                 if (kb.ask("EXIST(s), WUMPUS(s)")) {
-                    //Find and kill wumpus via RHW
+                    RHWTraversal("Facing Wumpus");
+                    move(5);    //shoot arrow
+                    move(2);    //move forward
                 } else {
-                    //explore potentially safe space    
+                    //explore potentially unsafe space
+                    //we should never reach this step since were only shooting at a Wumpus who's location we know?
                 }
             }
         }
+    }
+
+    private void RHWTraversal(String stopCondition) {
+
+        do {
+            if (kb.ask("right is safe")) {
+                move(4);        //turn right
+            } else {
+                if (kb.ask("forward is safe")) {
+                    move(2);   //go forward
+                } else {
+                    move(3);    //turn left
+                }
+            }
+            //if space to the right is safe --> turn right
+            //if space to right is unsafe && forward is safe --> go forward
+            //if space to right is unsafe && forward is unsafe --> turn left
+        } while (!kb.ask(stopCondition));
+
+        //face stop condition
+        while (!kb.ask("am i facing the stop condition?")) {
+            move(3);   //turn until facing stp condition
+        }
+        move(2);
     }
 
 }
