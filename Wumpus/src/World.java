dfcf5c342d8d385e72f229cee481f3c1f0244139
Zach Connelly <zmconnelly@gmail.com>
2016-10-20 16:48:10 -0600

    kek

--- Wumpus/src/World.java
+++ Wumpus/src/World.java
@@ -7,9 +7,10 @@ public final class World {
 
     public static final int NORTH = 1, EAST = 2, SOUTH = 3, WEST = 4;
     public static final int GRAB = 1, MOVE = 2, TURN_LEFT = 3, TURN_RIGHT = 4, SHOOT = 5, QUIT = 6;
+    
     protected final byte BREEZE = 0b00000001, STENCH = 0b0000010, BUMP = 0b00000100, GLITTER = 0b00001000, DEATH = 0b00010000, DEATH_WUMPUS = 0b00100000, SCREAM = 0b01000000;
-
     protected int arrowCount, x, y, direction = 0, score = 0, numMoves;
+    
     public static int size;
     private byte[][] perceptMap;
 
