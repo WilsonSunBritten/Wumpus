af70d655b66b06b53a4ddf0517f52d9d5de59ae0
Zach Connelly <zmconnelly@gmail.com>
2016-10-21 08:41:41 -0600

    goTo mostly working?  its getting called way too often though so weird things are happening...investingating

--- Wumpus/src/LogicExplorer.java
+++ Wumpus/src/LogicExplorer.java
@@ -14,7 +14,7 @@ public class LogicExplorer extends Agent {
     private Location safeSpace;
     private Location wumpusSpace;
     private ArrayList<Integer> moveHistory = new ArrayList<>();
-    boolean notFirstMove =  false;
+    boolean notFirstMove = false;
 
     public LogicExplorer(World world, int startingArrows, int startingX, int startingY, int direction) {
         super(world, startingArrows, startingX, startingY, direction);
@@ -30,36 +30,37 @@ public class LogicExplorer extends Agent {
         if (location.x > 0) {
             frontier.add(new Location(location.x - 1, location.y));
         }
-        if (location.x < world.size-1) {
+        if (location.x < world.size - 1) {
             frontier.add(new Location(location.x + 1, location.y));
         }
         if (location.y > 0) {
             frontier.add(new Location(location.x, location.y - 1));
         }
-        if (location.y < world.size-1) {
+        if (location.y < world.size - 1) {
             frontier.add(new Location(location.x, location.y + 1));
         }
     }
 
-    public void updateLocation(){
-        if(notFirstMove)
+    public void updateLocation() {
+        if (notFirstMove) {
             super.updateLocation();
-        else
+        } else {
             notFirstMove = true;
+        }
         expandFrontier();
     }
 
     public void expandFrontier() {
-        if (location.x > 0 && !searchedPositions[location.x-1][location.y]) {
+        if (location.x > 0 && !searchedPositions[location.x - 1][location.y]) {
             frontier.add(new Location(location.x - 1, location.y));
         }
-        if (location.x < world.size -1&& !searchedPositions[location.x+1][location.y]) {
+        if (location.x < world.size - 1 && !searchedPositions[location.x + 1][location.y]) {
             frontier.add(new Location(location.x + 1, location.y));
         }
-        if (location.y > 0 && !searchedPositions[location.x][location.y-1]) {
+        if (location.y > 0 && !searchedPositions[location.x][location.y - 1]) {
             frontier.add(new Location(location.x, location.y - 1));
         }
-        if (location.y < world.size -1&& !searchedPositions[location.x][location.y+1]) {
+        if (location.y < world.size - 1 && !searchedPositions[location.x][location.y + 1]) {
             frontier.add(new Location(location.x, location.y + 1));
         }
     }
@@ -90,13 +91,13 @@ public class LogicExplorer extends Agent {
             case TURN_LEFT:
                 moveHistory.add(TURN_LEFT);
                 System.out.println("Turning left");
-                direction = (direction + 3)%4;
+                direction = (direction + 3) % 4;
                 world.action(TURN_LEFT);
                 break;
             case TURN_RIGHT:
                 moveHistory.add(TURN_RIGHT);
                 System.out.println("turning right");
-                direction = (direction+1)%4;
+                direction = (direction + 1) % 4;
                 world.action(TURN_RIGHT);
                 break;
             case SHOOT:
@@ -132,8 +133,9 @@ public class LogicExplorer extends Agent {
             updateLocation();
             searchedPositions[location.x][location.y] = true;
         }
-        if((percepts&BUMP)!=0)
-            kb.tell(new Fact("Obsticle",getForward().x,false,getForward().y,false,false,null,null));
+        if ((percepts & BUMP) != 0) {
+            kb.tell(new Fact("Obsticle", getForward().x, false, getForward().y, false, false, null, null));
+        }
         if ((percepts & STENCH) != 0) {
             kb.tell(new Fact("Stench", location.x, false, location.y, false, true, null, null));
         } else {
@@ -176,108 +178,110 @@ public class LogicExplorer extends Agent {
             turnToSpace(safeSpace);
             move(MOVE);
             removeFromFrontier(safeSpace);
-        } 
-        else if(arrowCount > 0 && wumpusInFrontier()){
+        } else if (arrowCount > 0 && wumpusInFrontier()) {
             rhwTraversal(neighborSafeSpace(wumpusSpace));
             turnToSpace(wumpusSpace);
             move(SHOOT);
             move(MOVE);
             removeFromFrontier(wumpusSpace);
-        }
-        else {
-            rhwTraversal(neighborSafeSpace(frontier.get(frontier.size()-1)));
-            turnToSpace(frontier.get(frontier.size()-1));
-            frontier.remove(frontier.size()-1);
+        } else {
+            rhwTraversal(neighborSafeSpace(frontier.get(frontier.size() - 1)));
+            turnToSpace(frontier.get(frontier.size() - 1));
+            frontier.remove(frontier.size() - 1);
             move(MOVE);
         }
     }
-    
-    public void turnToSpace(Location loc){
-        if(location.x > loc.x){
-            if(direction == EAST)
+
+    public void turnToSpace(Location loc) {
+        if (location.x > loc.x) {
+            if (direction == EAST) {
                 return;
-            else if(direction == NORTH)
+            } else if (direction == NORTH) {
                 move(TURN_LEFT);
-            else if(direction == SOUTH)
-                move(TURN_RIGHT);
-        }
-        else if(loc.x > location.x){
-            if(direction == WEST)
-                return;
-            else if(direction == NORTH)
-                move(TURN_RIGHT);
-            else if(direction == SOUTH)
-                move(TURN_LEFT);
-        }
-        else if(loc.y < location.y){
-            if(direction == SOUTH)
-                return;
-            else if(direction == WEST)
-                move(TURN_LEFT);
-            else if(direction == EAST)
-                move(TURN_RIGHT);
-        }
-        else if(loc.y > location.y){
-            if(direction == NORTH){
-                return;
-            }
-            else if(direction == WEST){
+            } else if (direction == SOUTH) {
                 move(TURN_RIGHT);
             }
-            else if(direction == EAST)
+        } else if (loc.x > location.x) {
+            if (direction == WEST) {
+                return;
+            } else if (direction == NORTH) {
+                move(TURN_RIGHT);
+            } else if (direction == SOUTH) {
                 move(TURN_LEFT);
+            }
+        } else if (loc.y < location.y) {
+            if (direction == SOUTH) {
+                return;
+            } else if (direction == WEST) {
+                move(TURN_LEFT);
+            } else if (direction == EAST) {
+                move(TURN_RIGHT);
+            }
+        } else if (loc.y > location.y) {
+            if (direction == NORTH) {
+                return;
+            } else if (direction == WEST) {
+                move(TURN_RIGHT);
+            } else if (direction == EAST) {
+                move(TURN_LEFT);
+            }
         }
     }
-    
-    private Location neighborSafeSpace(Location location){
+
+    private Location neighborSafeSpace(Location location) {
         Location loc = null;
-        if(location.x > 0 && searchedPositions[location.x-1][location.y]){
-                loc = new Location(location.x-1,location.y);
-            
+        if (location.x > 0 && searchedPositions[location.x - 1][location.y]) {
+            loc = new Location(location.x - 1, location.y);
+
+        } else if (location.x < World.size - 1 && searchedPositions[location.x + 1][location.y]) {
+            loc = new Location(location.x + 1, location.y);
+        } else if (location.y > 0 && searchedPositions[location.x][location.y - 1]) {
+            loc = new Location(location.x, location.y - 1);
+        } else if (location.y < World.size - 1 && searchedPositions[location.x][location.y + 1]) {
+            loc = new Location(location.x, location.y + 1);
         }
-        else if(location.x < World.size -1 && searchedPositions[location.x+1][location.y])
-            loc = new Location(location.x+1,location.y);
-        else if(location.y > 0 && searchedPositions[location.x][location.y-1])
-            loc = new Location(location.x,location.y-1);
-        else if(location.y < World.size -1 && searchedPositions[location.x][location.y+1])
-            loc = new Location(location.x,location.y+1);
-        
+
         return loc;
     }
-    
-    private boolean wumpusInFrontier(){
-        for(Location loc : frontier){
-            if(kb.ask(new Fact("Wumpus", loc.x, false, loc.y, false, false, null, null))){
+
+    private boolean wumpusInFrontier() {
+        for (Location loc : frontier) {
+            if (kb.ask(new Fact("Wumpus", loc.x, false, loc.y, false, false, null, null))) {
                 wumpusSpace = loc;
                 return true;
             }
         }
-        
+
         return false;
     }
 
-    private void moveHistoryTraversal(Location loc){
-        if(loc.x == location.x && loc.y == location.y){
+    private void moveHistoryTraversal(Location loc) {
+        if (loc.x == location.x && loc.y == location.y) {
             return;
         }
-        for(int i = moveHistory.size()-1;i>=0; i--){
-            if(loc.x == location.x && loc.y == location.y){
+        for (int i = moveHistory.size() - 1; i >= 0; i--) {
+            if (loc.x == location.x && loc.y == location.y) {
                 return;
             }
             int move = moveHistory.get(i);
-            if(move == MOVE){
+            if (move == MOVE) {
                 move(TURN_LEFT);
                 move(TURN_LEFT);
                 move(MOVE);
-            }
-            else if(move == TURN_LEFT)
+            } else if (move == TURN_LEFT) {
                 move(TURN_RIGHT);
-            else if(move == TURN_RIGHT)
+            } else if (move == TURN_RIGHT) {
                 move(TURN_LEFT);
+            }
         }
     }
+
     private void rhwTraversal(Location location) {
-        moveHistoryTraversal(location);
+        //moveHistoryTraversal(location);
+        if (this.location != location) {
+            goTo(location.x, location.y);
+        }
+
         //go to location zach NOOOO!
     }
 
@@ -292,11 +296,10 @@ public class LogicExplorer extends Agent {
                     }
                 }
             }
-            if ( kb.ask(new Fact("Pit", loc.x, false, loc.y, false, false, null, null)) || kb.ask(new Fact("Obsticle", loc.x, false, loc.y, false, false, null, null))) {
+            if (kb.ask(new Fact("Pit", loc.x, false, loc.y, false, false, null, null)) || kb.ask(new Fact("Obsticle", loc.x, false, loc.y, false, false, null, null))) {
                 //there is specifically a wumpus, pit, or obsticle at this position, don't navigate to it.
                 frontier.remove(i);
-            }
-            else if(arrowCount == 0 && kb.ask(new Fact("Wumpus", loc.x, false, loc.y, false, false, null, null))){
+            } else if (arrowCount == 0 && kb.ask(new Fact("Wumpus", loc.x, false, loc.y, false, false, null, null))) {
                 frontier.remove(i);
             }
         }
@@ -309,20 +312,19 @@ public class LogicExplorer extends Agent {
         path = searchForPath(location.x, location.y, x, y, path);
         traversePath(path);
     }
-    
+
     private ArrayList<Location> searchForPath(int curX, int curY, int goalX, int goalY, ArrayList<Location> path) {
-        
 
         if (searchNext(curX, curY, goalX, goalY, path)) {
             return path;
         } else {
             System.out.println("Path finding error, no path found.");
-            return null;
+            return path;
         }
     }
-    
+
     private boolean searchNext(int curX, int curY, int goalX, int goalY, ArrayList<Location> path) {
-        
+
         path.add(new Location(curX, curY));
         if (curX == goalX && curY == goalY) {
             return true;
@@ -339,21 +341,51 @@ public class LogicExplorer extends Agent {
         path.remove(path.size() - 1);
         return false;
     }
-    
+
     private boolean isValid(int x, int y) {
-        
+
         if (x >= 0 && y >= 0 && x < World.size && y < World.size) {
             return searchedPositions[x][y];
         } else {
             return false;
         }
     }
-    
+
     private void traversePath(ArrayList<Location> path) {
-        
-        while (!path.isEmpty()) {
+
+        while (path.size() > 0) {
             Location next = path.remove(0);
-            //go to next
+            //turn to face
+            if (next.x > this.location.x) {
+                //go east
+                turnToSpace(next);
+                move(MOVE);
+                //world.action(MOVE);
+            } else if (next.x < this.location.x) {
+                //go west
+                turnToSpace(next);
+                move(MOVE);
+                //world.action(MOVE);
+            } else if (next.y > this.location.y) {
+                //go north
+                turnToSpace(next);
+                move(MOVE);
+                //world.action(MOVE);
+            } else if (next.y < this.location.y) {
+                //go south
+                turnToSpace(next);
+                move(MOVE);
+                //world.action(MOVE);
+            } else {
+                System.out.println("Error traversing path.");
+            }
+        }
+    }
+
+    private void turnToFace(int direction) {
+
+        while (this.direction != direction) {
+            turnLeft();
         }
     }
 }
