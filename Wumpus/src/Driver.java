7b86dc5551ddfa8f403488150fb26a5c2ee8701a
Zach Connelly <zmconnelly@gmail.com>
2016-10-20 22:25:29 -0600

    fe

--- Wumpus/src/Driver.java
+++ Wumpus/src/Driver.java
@@ -18,23 +18,24 @@ public class Driver {
 
     public static void makeGame() throws IOException {
 
-//        BufferedReader dataIn = new BufferedReader(new InputStreamReader(System.in));
-//        int[] prob = new int[3];
-//        System.out.print("% chance of generating pit: ");
-//        prob[0] = Integer.parseInt(dataIn.readLine());
-//        System.out.println();
-//        System.out.print("% chance of generating obstacle: ");
-//        prob[1] = Integer.parseInt(dataIn.readLine());
-//        System.out.println("");
-//        System.out.print("% chance of generating wumpus: ");
-//        prob[2] = Integer.parseInt(dataIn.readLine());
-//        System.out.println("");
-//
-//        WumpusGame game = new WumpusGame(5, prob);
+        BufferedReader dataIn = new BufferedReader(new InputStreamReader(System.in));
+        int[] prob = new int[3];
+        System.out.print("% chance of generating pit: ");
+        prob[0] = Integer.parseInt(dataIn.readLine());
+        System.out.println();
+        System.out.print("% chance of generating obstacle: ");
+        prob[1] = Integer.parseInt(dataIn.readLine());
+        System.out.println("");
+        System.out.print("% chance of generating wumpus: ");
+        prob[2] = Integer.parseInt(dataIn.readLine());
+        System.out.println("");
+
+        WumpusGame game = new WumpusGame(5, prob);
 
         //Agent explorer = new ReactiveExplorer(world, world.getLocation(), world.direction,world.getPercepts(), world.arrowCount);
         World world = new World("PerceptBoard.txt");
-        world.startGame("LogicExplorer");
+        //world.startGame("LogicExplorer");
+        world.startGame("ReactiveExplorer");
         //Agent explorer = new LogicExplorer(world);
 
         //world = new World("PerceptBoard.txt");
