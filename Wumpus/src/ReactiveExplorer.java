77278e5cd7a1a30dd94590f4d8ff8bf5fa8124c3
SirLagsalot <zmconnelly@gmail.com>
2016-10-23 17:30:57 -0600

    Theres a problem right now with not keeping track of location properly...also there are state conflicts between setting a space with a bump to be unsafe, but if an adjacent space is entered with no stench or breeze that gets overwritten

--- Wumpus/src/ReactiveExplorer.java
+++ Wumpus/src/ReactiveExplorer.java
@@ -54,13 +54,11 @@ public class ReactiveExplorer extends Agent {
                     turnLeft();
                     percepts = world.action(MOVE);
                     processPercepts();
-                    turnRight();
                     break;
                 case 2:     //try go right
                     turnRight();
                     percepts = world.action(MOVE);
                     processPercepts();
-                    turnLeft();
                     break;
                 default:    //turn around
                     turnRight();
@@ -90,30 +88,23 @@ public class ReactiveExplorer extends Agent {
                 switch (rand) {
                     case 0:
                         percepts = world.action(MOVE);
-                        if ((percepts & BUMP) != BUMP) {
-                            return;
-                        }
+                        processPercepts();
                         break;
                     case 1:
                         turnLeft();
                         percepts = world.action(MOVE);
-                        if ((percepts & BUMP) != BUMP) {
-                            return;
-                        }
-                        turnRight();
+                        processPercepts();
                         break;
                     case 2:
                         turnRight();
                         percepts = world.action(MOVE);
-                        if ((percepts & BUMP) != BUMP) {
-                            return;
-                        }
-                        turnLeft();
+                        processPercepts();
                         break;
                     default:
                         turnRight();
                         turnRight();
                         percepts = world.action(MOVE);
+                        processPercepts();
                         break;
                 }
             }
@@ -123,7 +114,6 @@ public class ReactiveExplorer extends Agent {
     private void processPercepts() {
 
         if ((percepts & BUMP) == BUMP) {
-            
             Location forward = getForward();
             if (forward.x >= 0 && forward.x < World.size && forward.y >= 0 && forward.y < World.size) {
                 safeMap[forward.x][forward.y] = false;
@@ -135,6 +125,9 @@ public class ReactiveExplorer extends Agent {
             if (forward.x >= 0 && forward.x < World.size && forward.y >= 0 && forward.y < World.size) {
                 safeMap[forward.x][forward.y] = false;
             }
+        } else {
+            this.prevLocation = this.location;
+            this.location = getForward();
         }
     }
 
@@ -161,105 +154,33 @@ public class ReactiveExplorer extends Agent {
 
     private void updateSafe() {
 
-        if (location.x < World.size - 1) {
-            safeMap[location.x + 1][location.y] = true;
+        try {
+            if (location.x < World.size - 1) {
+                safeMap[location.x + 1][location.y] = true;
+            }
+            if (location.x > 0) {
+                safeMap[location.x - 1][location.y] = true;
+            }
+            if (location.y < World.size - 1) {
+                safeMap[location.x][location.y + 1] = true;
+            }
+            if (location.y > 0) {
+                safeMap[location.x][location.y - 1] = true;
+            }
+        } catch (Exception e) {
+            System.out.println("Location: " + location.x + ", " + location.y);
+            throw e;
         }
-        if (location.x > 0) {
-            safeMap[location.x - 1][location.y] = true;
-        }
-        if (location.y < World.size - 1) {
-            safeMap[location.x][location.y + 1] = true;
-        }
-        if (location.y > 0) {
-            safeMap[location.x][location.y - 1] = true;
-        }
+
     }
 
-//    private byte move(int action) {
-//
-//        switch (action) {
-//            case GRAB:
-//                return world.action(GRAB);
-//            case MOVE:
-//                percepts = world.action(MOVE);
-//                if ((percepts & BUMP) != BUMP) {                            //did not bump into anything
-//                    prevLocation = location;
-//                    prevState = curState;
-//                    updateLocation();
-//                } else if ((percepts & DEATH_WUMPUS) == DEATH_WUMPUS) {     //killed by a wumpus, therefore use revive potion and take revenge
-//                    killWumpus();
-//                } else if ((percepts & DEATH) == DEATH) {                   //killed by a pit
-//                    Location temp = location;
-//                    location = prevLocation;
-//                    prevLocation = temp;
-//                    prevState = State.UNSAFE;
-//                    curState = State.EXPLORED;
-//                }
-//                return percepts;
-//            case TURN_LEFT:
-//                turnLeft();
-//                return world.action(TURN_LEFT);
-//            case TURN_RIGHT:
-//                turnRight();
-//                return world.action(TURN_RIGHT);
-//            case SHOOT:
-//                arrowCount--;
-//                return world.action(SHOOT);
-//            case QUIT:
-//                return world.action(QUIT);
-//        }
-//        return 0b00000000;
-//    }
-//    public void decideNextAction() {    //select safe neighboring cell else select unsafe neighboring cell
-//
-//        if (((percepts & STENCH) != 0) && ((percepts & BREEZE) != 0)) {        //all adjacent spaces are safe
-//            int rand = random.nextInt(3);
-//            switch (rand) {
-//                case 0:                 //go forward
-//                    move(MOVE);
-//                    break;
-//                case 1:                 //turn left and go forward
-//                    move(TURN_LEFT);
-//                    move(MOVE);
-//                    break;
-//                case 2:                 //turn right and go forward
-//                    move(TURN_RIGHT);
-//                    move(MOVE);
-//                    break;
-//                default:
-//                    System.out.println("Invalid case: random action, reactive explorer (safe) rand = " + rand);
-//            }
-//        } else {                                    //neighboring cells may not be safe
-//
-//            if (prevState == State.SAFE) {          //there might be a situation where the agent move back and forth between 3 safe safe spaces here we might need to account for
-//                move(TURN_LEFT);
-//                move(TURN_LEFT);
-//                move(MOVE);
-//            } else {                                //pick random move
-//                int rand = random.nextInt(3);
-//                switch (random.nextInt(3)) {
-//                    case 0:                         //go forward
-//                        move(MOVE);
-//                        break;
-//                    case 1:                         //turn left and go forward
-//                        move(TURN_LEFT);
-//                        move(MOVE);
-//                        break;
-//                    case 2:                         //turn right and go forward
-//                        move(TURN_RIGHT);
-//                        move(MOVE);
-//                        break;
-//                    default:
-//                        System.out.println("Invalid case: random action, reactive explorer (unsafe) rand = " + rand);
-//                }
-//            }
-//        }
-//    }
     private void killWumpus() {
-        System.out.println("kill wumpus");
-        location = prevLocation;
+
+        System.out.println("Killing wumpus..");
         percepts = world.getPercepts();
         world.action(SHOOT);
         world.action(MOVE);
+        this.prevLocation = this.location;
+        this.location = getForward();
     }
 }
